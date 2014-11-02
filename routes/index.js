58c63ed9c384a364674fea9c5467aee7c2d5733f
Pompeu <itacir@hotmail.com>
2014-11-02 02:37:11 -0200

    json funcionando

--- routes/index.js
+++ routes/index.js
@@ -2,29 +2,40 @@ var express = require('express'),
 	router 	= express.Router(),
 	jsdom 	= require('jsdom'),
 	fs		= require('fs'),
+	objToJson,
 	jquery 	= fs.readFileSync("public/javascripts/jquery.js", "utf-8");
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/atualizar', function(req, res) {
  	
  	  jsdom.env({
         url: "http://ifgoiano.edu.br/morrinhos/home/index.php",
         src: [jquery],
         done: function (errors, window) {
-           var $ = window.$;
-           var objToJson = [ ];
-           var titulo = "";
-           $("h2:not(:last)").each(function (){
+           	var $ = window.$;
+           	objToJson = [ ];
+           	var titulo = "";
+           	$("h2:not(:last)").each(function (){
            		titulo = myTrim($(this).text());
            		objToJson.push({ "Titulo" : titulo });  
-           });
-           $("p:not(:last)" ).each(function (){
+           	});
+           	$("p:not(:last)" ).each(function (){
            	 	objToJson.push({ "Texto" : $(this).text() });  
-           });
-           return res.json(objToJson);
+           	});
+		    fs.writeFile("noticias.json", JSON.stringify(objToJson) , function(err) {
+		    	if(err) {
+		        	console.log(err);
+		    	} else {
+		        	console.log("The file was saved!");
+		    	}
+			});
+           	return res.json(objToJson);
         }
       });      
 	 
 });
+
+
+//class="blog_more"
 /*
 	Thx to W3c Schol for this
 	algoritmn
@@ -33,5 +44,12 @@ router.get('/', function(req, res) {
 function myTrim(x) {
     return x.replace(/^\s+|\s+$/gm,'');
 }
+
+router.get('/' , function(req , res){
+var dados = fs.readFileSync('noticias.json');
+	
+	res.json(JSON.parse(dados))
+})
+ 
 
 module.exports = router;
