a2b7c3e541fba47914f73434e7511ae94eab275c
Pompeu <itacir@hotmail.com>
2014-11-03 17:38:53 -0200

    json donwload

--- routes/index.js
+++ routes/index.js
@@ -3,35 +3,8 @@ var express = require('express'),
 	jsdom 	= require('jsdom'),
 	fs		= require('fs'),
 	objToJson,
-	jquery 	= fs.readFileSync("public/javascripts/jquery.js", "utf-8");
-/* GET home page. */
-/*router.get('/heoaheoaheohahehaehahehae12391237129837', function(req, res) {
- 	
- 	  jsdom.env({
-        url: "http://ifgoiano.edu.br/morrinhos/home/index.php",
-        src: [jquery],
-        done: function (errors, window) {
-           	var $ = window.$;
-           	objToJson = [ ];
-           	var titulo = "";
-           	$("h2:not(:last)").each(function (){
-           		titulo = myTrim($(this).text());
-           		objToJson.push({ "Titulo" : titulo });  
-           	});
-           	$("p:not(:last)" ).each(function (){
-           	 	objToJson.push({ "Texto" : $(this).text() });  
-           	});
-		    fs.writeFile("noticias.json", JSON.stringify(objToJson) , function(err) {
-		    	if(err) {
-		        	console.log(err);
-		    	} else {
-		        	console.log("The file was saved!");
-		    	}
-			});
-           	return res.json(objToJson);
-        }
-      });      
-});*/
+	jquery 	= fs.readFileSync("public/javascripts/jquery-min.js", "utf-8");
+
 function atualizar(){
 	 jsdom.env({
         url: "http://ifgoiano.edu.br/morrinhos/home/index.php",
@@ -56,7 +29,7 @@ function atualizar(){
 			});
            	return objToJson;
         }
-      }); 
+      });
 }
 //class="blog_more"
 /*
@@ -68,20 +41,25 @@ function myTrim(x) {
     return x.replace(/^\s+|\s+$/gm,'');
 }
 
+var atualizarJson = function(time , callback, outer){
+
+	outer();
+	var stop = new Date().getTime();
+    while(new Date().getTime() < stop + time) {
+        
+    }
+    callback();
+}
 router.get('/' , function(req , res){
 	
-	var a = new Date();
-	
-	if(a.getMinutes() == 15 || a.getHours() == 45 ){
-		atualizar();
-		return res.json(objToJson);
-		
-	}
 	var dados = fs.readFileSync('noticias.json');
-	 	return res.json(JSON.parse(dados));
-		
+	 
+	atualizarJson(1000, atualizar, res.json(JSON.parse(dados)));
+	
 	
 });
- 
+router.get('/json' , function(req , res){ 
+	res.send(fs.readFileSync('noticias.json'))	
+});
 
 module.exports = router;
