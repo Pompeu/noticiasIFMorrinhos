ae1a3482b54ad34372f6499c39d91548dce48fb7
Pompeu <itacir@hotmail.com>
2015-03-06 01:23:12 -0300

    localstorege for off line ok

---
+++ AppCordova/src/main.js
@@ -0,0 +1,25 @@
+angular.module('App', [])
+  .controller('MainCtrl', ['$scope','$http', function($scope, $http){
+    $scope.noticias = null;
+    $http.get('https://pure-shore-2163.herokuapp.com/noticias')
+      .success(function(data) {
+        $scope.noticias = data.noticias;
+        localStorage.setItem('noticias',  JSON.stringify($scope.noticias));
+        localStorage.setItem('minuto',new Date().getMinutes());
+      })
+      .error(function() {
+        $scope.noticias = JSON.parse(localStorage.getItem('noticias'));
+      })
+  }])
+  .controller('ListContatoCtrl', ['$scope','$http', function ($scope,$http) {
+    $scope.items = null;
+    $http.get('contato.json')
+      .success(function(data) {
+        setInterval(function() {
+          $scope.items = data;
+        }, 200)       
+      })
+  }])
+
+
+
